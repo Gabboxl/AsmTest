01d7278582aa8b35768e40699214fb70772ce828
Gabriele <gabboxl0@gmail.com>
2025-05-07 00:17:29 +0200

    nuovi test

--- AsmTest/AsmTest.cpp
+++ AsmTest/AsmTest.cpp
@@ -1,5 +1,3 @@
-// AsmTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
--- AsmTest/sweg.asm
+++ AsmTest/sweg.asm
@@ -1,7 +1,17 @@
 .data
+swagNum dq 7
+swagMex dq "bella", 0
+
 .code
 TestFunction proc
 	mov rax, 89
+
+	mov rax, swagNum
+	mov rax, [swagNum] ; uguale al precedente
+
+	lea rax, swagNum
+	lea rax, [swagNum]  ; uguale al precedente
+
 
 	ret
 TestFunction endp
